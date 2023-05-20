743b20749772fdd517a871bb87f4fbdb08f05c27
Parviz728 <pnorov5@gmail.com>
2023-05-20 14:53:25 +0300

    Initial commit

--- task1_solution/task1_query1.sql
+++ task1_solution/task1_query1.sql
@@ -1,5 +1,5 @@
 select press_city, count(*) as num_of_books from book
-where extract(year from date_of_comming) = 2016
+where extract(year from date_of_comming) = 2021
 group by press_city
 HAVING count(*) > 0
 order by num_of_books DESC
